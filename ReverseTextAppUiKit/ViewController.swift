d23dec51cd31b0be610201cd9d7628083b107622
Alex <gao.sanya@gmail.com>
2022-06-17 17:22:18 +0300

    init

--- ReverseTextAppUiKit/ViewController.swift
+++ ReverseTextAppUiKit/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate{
     
     @IBAction func ReverseButton(_ sender: Any) {
         var reversed: String = ""
+        
         var tempString: String = ""
         
         if TextField.text != nil {
