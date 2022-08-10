832ade40598f81260d8884153148812a34c6b6ed
Alex <gao.sanya@gmail.com>
2022-08-10 15:59:12 +0300

    first try for testing

--- ReverseTextAppUiKitUITests/ReverseTextAppUiKitUITests.swift
+++ ReverseTextAppUiKitUITests/ReverseTextAppUiKitUITests.swift
@@ -11,35 +11,29 @@ import XCTest
 
 class ReverseTextAppUiKitUITests: XCTestCase {
     
-    var sut: ReverseWordsViewController!
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut = ReverseWordsViewController()
     }
     
     override func tearDownWithError() throws {
-        sut = nil
         try super.tearDownWithError()
     }
     
     func test1() throws {
-        /*let yourStoryboard = UIStoryboard(name: "ReverseTextAppUiKit", bundle: nil)
-         let yourViewController = yourStoryboard.instantiateViewController(withIdentifier: "ReverseWordsViewController") as! ReverseWordsViewController
-         yourViewController.loadView()
-         */
         let app = XCUIApplication()
         let enterTextTextField = app.textFields["Enter text:"]
         enterTextTextField.tap()
         let lable = app.staticTexts["Lable"]
         app.textFields.element.typeText("Hello World!")
-        XCTAssertEqual(app.buttons.element.title, "Reverse")
+        app.keyboards.buttons["return"].tap()
+        XCTAssertEqual(app.buttons.element.label, "Reverse")
         let reverseStaticText = app/*@START_MENU_TOKEN@*/.staticTexts["Reverse"]/*[[".buttons[\"Reverse\"].staticTexts[\"Reverse\"]",".staticTexts[\"Reverse\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         reverseStaticText.tap()
-        XCTAssertEqual(lable.title, "olleH !dlroW")
-        XCTAssertEqual(app.buttons.element.title, "Clear")
+        XCTAssertEqual(lable.label, "olleH !dlroW")
+        XCTAssertEqual(app.buttons.element.label, "Clear")
         let clearStaticText = app/*@START_MENU_TOKEN@*/.staticTexts["Clear"]/*[[".buttons[\"Clear\"].staticTexts[\"Clear\"]",".staticTexts[\"Clear\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         clearStaticText.tap()
-        XCTAssertEqual(app.buttons.element.title, "Reverse")
+        XCTAssertEqual(app.buttons.element.label, "Reverse")
     }
     
     
